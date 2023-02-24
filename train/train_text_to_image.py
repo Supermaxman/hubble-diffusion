95ff09a78199dbf65ed03cd4d29337075f5cec72
Max Weinzierl <supermaxman1@gmail.com>
2023-02-24 00:53:56 -0600

    Added streaming for large dataset

--- train/train_text_to_image.py
+++ train/train_text_to_image.py
@@ -209,7 +209,7 @@ def parse_args():
     parser.add_argument(
         "--lr_warmup_steps",
         type=int,
-        default=500,
+        default=0,
         help="Number of steps for the warmup in the lr scheduler.",
     )
     parser.add_argument(
