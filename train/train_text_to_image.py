c3173ac91f32a1853b953c198449d64197f9e607
Max Weinzierl <supermaxman1@gmail.com>
2023-02-24 08:02:08 -0600

    Added streaming for large dataset

--- train/train_text_to_image.py
+++ train/train_text_to_image.py
@@ -31,13 +31,12 @@ import transformers
 from accelerate import Accelerator
 from accelerate.logging import get_logger
 from accelerate.utils import ProjectConfiguration, set_seed
-from datasets import load_dataset, DownloadConfig
+from datasets import load_dataset
 from huggingface_hub import HfFolder, Repository, create_repo, whoami
 from packaging import version
 from torchvision import transforms
+from tqdm.auto import tqdm
 from transformers import CLIPTextModel, CLIPTokenizer
-
-from tqdm.rich import tqdm
 import PIL
 from PIL import Image
 
@@ -54,6 +53,18 @@ from diffusers.optimization import get_scheduler
 from diffusers.training_utils import EMAModel
 from diffusers.utils import check_min_version, deprecate
 from diffusers.utils.import_utils import is_xformers_available
+import torchvision.transforms.functional as TF
+
+
+class ResizeIfSmaller:
+    def __init__(self, size):
+        self.size = size
+
+    def __call__(self, x):
+        w, h = x.size
+        if w < self.size or h < self.size:
+            return TF.resize(x, self.size)
+        return x
 
 
 # Will error if the minimal version of diffusers is not installed. Remove at your own risks.
@@ -94,12 +105,6 @@ def parse_args():
         default=None,
         help="The config of the Dataset, leave as None if there's only one config.",
     )
-    parser.add_argument(
-        "--dataset_streaming",
-        action="store_true",
-        help="Whether or not to use streaming for large datasets.",
-    )
-
     parser.add_argument(
         "--train_data_dir",
         type=str,
@@ -155,7 +160,6 @@ def parse_args():
             " resolution"
         ),
     )
-    # TODO add support for vertical and horizontal flips
     parser.add_argument(
         "--random_flip",
         action="store_true",
@@ -381,6 +385,11 @@ def get_full_repo_name(
         return f"{organization}/{model_id}"
 
 
+dataset_name_mapping = {
+    "lambdalabs/pokemon-blip-captions": ("image", "text"),
+}
+
+
 def main():
     args = parse_args()
 
@@ -587,14 +596,7 @@ def main():
             args.dataset_name,
             args.dataset_config_name,
             cache_dir=args.cache_dir,
-            streaming=args.dataset_streaming,
         )
-        train_dataset = dataset["train"]
-        if not args.dataset_streaming:
-            dataset_size = len(train_dataset)
-            train_dataset = train_dataset.to_iterable_dataset()
-        else:
-            dataset_size = train_dataset.info.splits["train"].num_examples
     else:
         data_files = {}
         if args.train_data_dir is not None:
@@ -604,22 +606,47 @@ def main():
             data_files=data_files,
             cache_dir=args.cache_dir,
         )
-        train_dataset = dataset["train"]
-        dataset_size = len(train_dataset)
-        train_dataset = train_dataset.to_iterable_dataset()
         # See more about loading custom images at
         # https://huggingface.co/docs/datasets/v2.4.0/en/image_load#imagefolder
 
+    # def check_image(example):
+    #     w, h = example["width"], example["height"]
+    #     if w < args.resolution or h < args.resolution:
+    #         return False
+    #     return True
+
     # Preprocessing the datasets.
-    image_column = args.image_column
-    caption_column = args.caption_column
+    # We need to tokenize inputs and targets.
+    column_names = dataset["train"].column_names
+
+    # 6. Get the column names for input/target.
+    dataset_columns = dataset_name_mapping.get(args.dataset_name, None)
+    if args.image_column is None:
+        image_column = (
+            dataset_columns[0] if dataset_columns is not None else column_names[0]
+        )
+    else:
+        image_column = args.image_column
+        if image_column not in column_names:
+            raise ValueError(
+                f"--image_column' value '{args.image_column}' needs to be one of: {', '.join(column_names)}"
+            )
+    if args.caption_column is None:
+        caption_column = (
+            dataset_columns[1] if dataset_columns is not None else column_names[1]
+        )
+    else:
+        caption_column = args.caption_column
+        if caption_column not in column_names:
+            raise ValueError(
+                f"--caption_column' value '{args.caption_column}' needs to be one of: {', '.join(column_names)}"
+            )
 
     # Preprocessing the datasets.
     # We need to tokenize input captions and transform the images.
     def tokenize_captions(examples, is_train=True):
         captions = []
-        for ex in examples:
-            caption = ex[caption_column]
+        for caption in examples[caption_column]:
             if isinstance(caption, str):
                 captions.append(caption)
             elif isinstance(caption, (list, np.ndarray)):
@@ -641,6 +668,7 @@ def main():
     # Preprocessing the datasets.
     train_transforms = transforms.Compose(
         [
+            ResizeIfSmaller(args.resolution),
             transforms.RandomCrop(args.resolution),
             # TODO add random vertical flip
             transforms.RandomHorizontalFlip(),
@@ -649,42 +677,37 @@ def main():
         ]
     )
 
-    def check_image(example):
-        w, h = example["width"], example["height"]
-        if w < args.resolution or h < args.resolution:
-            return False
-        return True
+    def preprocess_train(examples):
+        images = [image.convert("RGB") for image in examples[image_column]]
+        examples["pixel_values"] = [train_transforms(image) for image in images]
+        examples["input_ids"] = tokenize_captions(examples)
+        return examples
 
     with accelerator.main_process_first():
-        if args.max_train_samples is not None:
-            train_dataset = train_dataset.select(range(args.max_train_samples))
+        dataset["train"] = dataset["train"].shuffle(seed=args.seed)
+        # dataset["train"] = dataset["train"].filter(lambda example: check_image(example))
         # Set the training transforms
-        train_dataset = train_dataset.filter(lambda example: check_image(example))
-        train_dataset = train_dataset.shuffle(seed=args.seed, buffer_size=100)
+        train_dataset = dataset["train"].with_transform(preprocess_train)
 
     def collate_fn(examples):
-        pixel_values = torch.stack(
-            [train_transforms(ex[image_column]) for ex in examples]
-        )
+        pixel_values = torch.stack([example["pixel_values"] for example in examples])
         pixel_values = pixel_values.to(memory_format=torch.contiguous_format).float()
-        input_ids = tokenize_captions(examples)
+        input_ids = torch.stack([example["input_ids"] for example in examples])
         return {"pixel_values": pixel_values, "input_ids": input_ids}
 
     # DataLoaders creation:
     train_dataloader = torch.utils.data.DataLoader(
         train_dataset,
-        shuffle=False,
+        shuffle=True,
         collate_fn=collate_fn,
         batch_size=args.train_batch_size,
         num_workers=args.num_workers,
     )
 
-    dataloader_size = int(math.ceil(dataset_size / args.train_batch_size))
-
     # Scheduler and math around the number of training steps.
     overrode_max_train_steps = False
     num_update_steps_per_epoch = math.ceil(
-        dataloader_size / args.gradient_accumulation_steps
+        len(train_dataloader) / args.gradient_accumulation_steps
     )
     if args.max_train_steps is None:
         args.max_train_steps = args.num_train_epochs * num_update_steps_per_epoch
@@ -719,7 +742,7 @@ def main():
 
     # We need to recalculate our total training steps as the size of the training dataloader may have changed.
     num_update_steps_per_epoch = math.ceil(
-        dataloader_size / args.gradient_accumulation_steps
+        len(train_dataloader) / args.gradient_accumulation_steps
     )
     if overrode_max_train_steps:
         args.max_train_steps = args.num_train_epochs * num_update_steps_per_epoch
@@ -739,7 +762,7 @@ def main():
     )
 
     logger.info("***** Running training *****")
-    logger.info(f"  Num examples = {dataset_size}")
+    logger.info(f"  Num examples = {len(train_dataset)}")
     logger.info(f"  Num Epochs = {args.num_train_epochs}")
     logger.info(f"  Instantaneous batch size per device = {args.train_batch_size}")
     logger.info(
@@ -785,7 +808,6 @@ def main():
     progress_bar.set_description("Steps")
 
     for epoch in range(first_epoch, args.num_train_epochs):
-        train_dataset.set_epoch(epoch)
         unet.train()
         train_loss = 0.0
         for step, batch in enumerate(train_dataloader):
